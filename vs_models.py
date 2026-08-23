614854e353148d886e1462a2c108037eedda9374
hirobon1690 <58695125+hirobon1690@users.noreply.github.com>
2026-08-23 12:59:30 +0900

    Update vs_models.py

--- vs_models.py
+++ vs_models.py
@@ -1,6 +1,7 @@
 from src.ReversiGUI import ReversiGUI
 from models.cpu import cpu_lv0, cpu_lv1, cpu_lv2, cpu_lv3, cpu_lv4, cpu_lv5
-
+import random
+ 
 """
 CPUの手を選択する関数
 
@@ -21,6 +22,16 @@ CPUの手を選択する関数
 
 
 def cpu_algorithm(board, player_num):
+    all_list = [[i, j] for i in range(1, 9) for j in range(1, 9)]
+    A_list=[[1,1],[1,8],[8,1],[8,8]]
+    C_list=[[4,1],[5,1],[1,4],[1,5],[4,8],[5,8],[8,4],[8,5]]
+    D_list=[[2,4],[2,5],[4,2],[4,7],[5,2],[5,7],[7,4],[7,5]]
+    E_list=[[2,2],[2,7],[7,2],[7,7],[1,2],[2,1],[2,8],[1,7],[7,1],[8,2],[8,7],[7,8]]
+
+
+    B_list = [x for x in all_list if x not in A_list + C_list + D_list + E_list]
+    print(B_list)
+
     valid_moves = []  # 置けるマスかを格納するリスト
     for x in range(1, 9):  # 1行ずつ走査
         for y in range(1, 9):  # 1列ずつ走査
@@ -28,7 +39,48 @@ def cpu_algorithm(board, player_num):
                 board, player_num, x, y
             ):  # その(x,y)座標に石を置けるか判定
                 valid_moves.append([x, y])  # 置けるマスとしてリストに追加
+    print(valid_moves)
     if valid_moves != []:  # 置けるマスがある場合
+        ok_list = []
+        for item in A_list:
+            for valid_move in valid_moves:
+                if item == valid_move:
+                    ok_list.append(valid_move)	
+        if ok_list != []:
+            return random.choice(ok_list)
+    
+        ok_list = []
+        for item in B_list:
+            for valid_move in valid_moves:
+                if item == valid_move:
+                    ok_list.append(valid_move)	
+        if ok_list != []:
+            return random.choice(ok_list)
+        
+        ok_list = []
+        for item in C_list:
+            for valid_move in valid_moves:
+                if item == valid_move:
+                    ok_list.append(valid_move)	
+        if ok_list != []:
+            return random.choice(ok_list)
+
+        ok_list = []
+        for item in D_list:
+            for valid_move in valid_moves:
+                if item == valid_move:
+                    ok_list.append(valid_move)	
+        if ok_list != []:
+            return random.choice(ok_list)
+
+        ok_list = []
+        for item in E_list:
+            for valid_move in valid_moves:
+                if item == valid_move:
+                    ok_list.append(valid_move)	
+        if ok_list != []:
+            return random.choice(ok_list)
+        
         return valid_moves[0]
     return valid_moves
 
